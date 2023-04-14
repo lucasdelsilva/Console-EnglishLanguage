96522bdedcd04ea0dec12630e4089a7cf9975ae3
Lucas <silva.lucasdel@outlook.com>
2023-04-14 02:38:32 -0300

    Ajustes e lógica para MENU

--- EnglishLanguage/Program.cs
+++ EnglishLanguage/Program.cs
@@ -4,14 +4,11 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Olá, seja bem-vindo a um treinamento de inglês.");
-        Console.WriteLine("-----------------------------------------------");
-
+        Console.WriteLine("-----------------------MENU------------------------");
         while (true)
         {
-            Console.WriteLine();
-            Console.WriteLine("já sabe como funciona ? ---> Digite (Sim / Não)");
-            Console.WriteLine("Se caso quiser sair. ---> Digite (Sair)");
+            Console.WriteLine("1 - Se já sabe como funciona é so digitar (S).");
+            Console.WriteLine("2 - Se ainda não sabe é so digitar (N).");
             Console.WriteLine();
             Console.Write("Sua resposta é: ");
 
@@ -21,42 +18,54 @@ internal class Program
 
             switch (resposta)
             {
-                case "sim":
+                case "s":
                     LetsBora();
                     break;
-                case "não":
-                case "nao":
+                case "n":
                     ComoFunciona();
                     break;
                 case "sair":
                     Console.WriteLine();
                     Console.WriteLine("Finalizando...");
+                    Console.WriteLine("Até logo!");
                     return;
                 default:
-                    Console.WriteLine("-----------------------------------------------");
-                    Console.WriteLine("Resposta invalida, tente novamente: Aperte (ENTER)");
+                    Console.WriteLine("-----------------------MENU------------------------");
+                    Console.WriteLine("Resposta invalida, tente novamente!");
+                    Console.WriteLine();
                     break;
             }
-            Console.ReadKey();
         }
     }
 
     public static void ComoFunciona()
     {
         Console.WriteLine();
-        Console.WriteLine("------------COMO FUNCIONA-------------");
-        Console.WriteLine("Escolhemos frases aleatórias e você terá que traduzir as frases em (PORTUGUES) ou em (INGlÊS) da maneira correta.");
-        Console.Write("Por favor, para que sua frase esteja correta, deverá usar os caracteres especiais, EX: '.', '?', ',', '!' OU qualquer acento nas frases.");
+        Console.WriteLine("-------------------COMO FUNCIONA-------------------");
+        Console.WriteLine("1 - São diversas frases aleatórias.");
+        Console.WriteLine("2 - Você terá que traduzir da maneira correta.");
+        Console.WriteLine("3 - Português ----> Inglês OU Inglês ----> Português.");
+        Console.WriteLine("4 - Para que sua frase esteja correta, use os caracteres especiais e acentos assim como nas frases - ('.', '?', '!', '´', '~').");
+        Console.WriteLine("5 - Você poderá sair a qualquer momento é só digitar (Sair).");
         Console.WriteLine();
-        Console.WriteLine("------------EXEMPLOS-------------");
+        Console.WriteLine("----------------------EXEMPLOS---------------------");
         Console.WriteLine("1 - What's your name? ------> Qual é o seu nome?");
         Console.WriteLine("2 - Eu não estou dormindo agora. ------> I'm not sleeping now.");
         Console.WriteLine();
-        Console.WriteLine("Se quiser desistir é so digitar (Sair).");
-        Console.WriteLine("Agora vamos colocar em pratica!");
-        Console.WriteLine("--------------------------------------");
+        Console.WriteLine("---------------------BOA SORTE!--------------------");
+        Console.WriteLine("Agora vamos colocar em pratica! Se estiver pronto é so digitar (S), se não (N).");
+        Console.WriteLine();
 
-        LetsBora();
+        Console.Write("Sua resposta é: ");
+
+        var resposta = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine("-----------------------MENU------------------------");
+
+        if (resposta is not null && resposta.ToLower().Equals("s"))
+            LetsBora();
+        else
+            return;
     }
 
     public static void LetsBora()
@@ -70,7 +79,7 @@ internal class Program
         var listaSimplePresents = list.OrderBy(a => ramdom.Next()).ToList();
 
         Console.WriteLine("Se quiser desistir é só digitar (Sair)");
-        Console.WriteLine("------------EXERCICIOS-------------");
+        Console.WriteLine("----------------------EXERCICIOS---------------------");
         Console.WriteLine();
 
         //Simple present 
@@ -101,6 +110,5 @@ internal class Program
                 }
             }
         }
-        Console.ReadKey();
     }
 }
